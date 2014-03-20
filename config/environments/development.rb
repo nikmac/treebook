5ff6b329c57d4305dfb7d0e85be6047f51124276
nikmac <nikicrisler@gmail.com>
2014-03-20 09:52:33 -0700

    Install devise, add a user and install devise views.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Treebook::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+config.action_mailer.default_url_options = { host: '127.0.0.1:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Treebook::Application.routes.draw do
+  devise_for :users
   resources :statuses
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to=>'statuses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+#get '/statuses'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
